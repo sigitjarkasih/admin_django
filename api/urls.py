af42edc7737567b6553c3eeab53a880f8c3661ab
sigitjakarsih <sigitjarkasih99@gmail.com>
2023-01-05 22:09:17 +0700

    Perubahan Dokumentasi DGMall Syariah

--- api/urls.py
+++ api/urls.py
@@ -2,21 +2,21 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .urlsGroup import (
-    article_donasi, article_event, article_login, article_register, article_pembeli, article_penjual, article_dompetmadrasah, article_staycation, article_akadtransaksi, article_syariaharea
+   article_akadtransaksi, article_dompetmadrasah, article_donasi, article_event, article_login, article_pembeli, article_penjual, article_register, article_staycation, article_syariaharea
 )
 
 urlpatterns = [
-    #ADMIN
-    path("v1/donasu/", include(article_donasi.urlpatterns)),
+#ADMIN
+    path("v1/akad-transaksi/", include(article_akadtransaksi.urlpatterns)),
+    path("v1/dompet-madrasah/", include(article_dompetmadrasah.urlpatterns)),
+    path("v1/donasi/", include(article_donasi.urlpatterns)),
+    path("v1/event/", include(article_event.urlpatterns)),
     path("v1/login/", include(article_login.urlpatterns)),
-    path("v1/register/", include(article_register.urlpatterns)),
     path("v1/pembeli/", include(article_pembeli.urlpatterns)),
     path("v1/penjual/", include(article_penjual.urlpatterns)),
-    path("v1/dompet-madrasah/", include(article_dompetmadrasah.urlpatterns)),
-    path("v1/event/", include(article_event.urlpatterns)),
+    path("v1/register/", include(article_register.urlpatterns)),
     path("v1/staycation/", include(article_staycation.urlpatterns)),
-    path("v1/akad-transaksi/", include(article_akadtransaksi.urlpatterns)),
-    path("v1/syariah-area/", include(article_syariaharea.urlpatterns)),
+    path("v1/syariaharea/", include(article_syariaharea.urlpatterns)),
 ]
 
 if settings.DEBUG:
--- api/urlsGroup/article_donasi.py
+++ api/urlsGroup/article_donasi.py
@@ -1,12 +1,12 @@
 from django import urls
 from django.urls import path
 from api.viewsGroup.article_donasi.create import create
-from api.viewsGroup.article_donasi.delete import delete
+from api.viewsGroup.article_donasi.list import list
 from api.viewsGroup.article_donasi.listById import listById
 from api.viewsGroup.article_donasi.update import update
 from api.viewsGroup.article_donasi.imageUpload import imageUpload
 from api.viewsGroup.article_donasi.listActivate import listActive
-from api.viewsGroup.article_donasi.list import list
+from api.viewsGroup.article_donasi.delete import delete
 
 urlpatterns = [
     path("list/", list),
