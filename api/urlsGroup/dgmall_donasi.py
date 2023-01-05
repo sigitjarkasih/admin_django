455613b76fa3394966d6322ee543bbe9eb18eaf5
sigitjakarsih <sigitjarkasih99@gmail.com>
2023-01-05 20:19:48 +0700

    Donasi Nih

--- api/urlsGroup/dgmall_donasi.py
+++ api/urlsGroup/dgmall_donasi.py
@@ -1,12 +1,12 @@
 from django import urls
 from django.urls import path
-from api.viewsGroup.dgmall_donasi.create import create
-from api.viewsGroup.dgmall_donasi.list import list
-from api.viewsGroup.dgmall_donasi.listById import listById
-from api.viewsGroup.dgmall_donasi.update import update
-from api.viewsGroup.dgmall_donasi.imageUpload import imageUpload
-from api.viewsGroup.dgmall_donasi.listActive import listActive
-from api.viewsGroup.dgmall_donasi.delete import delete
+from api.viewsGroup.article_donasi.create import create
+from api.viewsGroup.article_donasi.list import list
+from api.viewsGroup.article_donasi.listById import listById
+from api.viewsGroup.article_donasi.update import update
+from api.viewsGroup.article_donasi.imageUpload import imageUpload
+from api.viewsGroup.article_donasi.listActivate import listActive
+from api.viewsGroup.article_donasi.delete import delete
 
 urlpatterns = [
     path("list/", list),
